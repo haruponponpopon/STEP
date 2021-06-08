9ad5d89bdb364571d0886408c8b9e2a29c318ed8
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-08 18:54:15 +0900

    Update kadai1.cpp

--- week5/kadai1.cpp
+++ week5/kadai1.cpp
@@ -7,7 +7,7 @@
 #include <iomanip>  //デバッグprint用
 
 //ファイルからx,y座標を読み込んでvectorに入れて返す
-std::vector<std::vector<double>> read_file(std::string file_name){
+std::vector<std::vector<double>> read_file(const std::string& file_name){
     std::vector<std::vector<double>> coordinate(2);
     std::ifstream file(file_name);
     if (!file) {
@@ -36,9 +36,10 @@ std::vector<std::vector<double>> read_file(std::string file_name){
     return coordinate;
 }
 
-double city_distance(double ax, double ay, double bx, double by){
+double city_distance(const double ax, const double ay, const double bx, const double by){
     return sqrt((ax-bx)*(ax-bx)+(ay-by)*(ay-by));
 }
+
 //貪欲法で一番近い都市を入れていく
 std::vector<int> choose_nearest_city(const std::vector<std::vector<double>>& coordinate){
     int city_count = (int)coordinate[0].size();
@@ -65,23 +66,33 @@ std::vector<int> choose_nearest_city(const std::vector<std::vector<double>>& coo
     return ans_city_order;
 }
 
+void write_file(const std::vector<int>& ans_city_order, const std::string& file_name){
+    std::string filename(file_name);
+    std::fstream file_out;
+
+    file_out.open(filename, std::ios_base::out);
+    if (!file_out.is_open()) {
+        std::cerr << "failed to open " << filename << '\n';
+        std::exit(1);
+    } else {
+        file_out << "index" << std::endl;
+        for (int i=0; i<(int)ans_city_order.size(); i++){
+            file_out << ans_city_order.at(i) << std::endl;
+        }
+        std::cout << "file write succeeded!" << std::endl;
+    }
+}
+
+
 int main(int argc, char *argv[]){
-    if (argc!=2) {
-        std::cerr << "put file name" << std::endl;
+    if (argc!=3) {
+        std::cerr << "put two file name" << std::endl;
         std::exit(1);
     }
+    /*ファイルの読み込み*/
     std::vector<std::vector<double>> coordinate = read_file(argv[1]);
-    // int city_count = (int)coordinate[0].size();
-    std::vector<int> ans_city_order = choose_nearest_city(coordinate);
-    for (int i=0; i<(int)ans_city_order.size(); i++){
-        std::cout << ans_city_order.at(i) << std::endl;
-    }
-    
-
-    // for (int i=0; i<(int)coordinate.size(); i++){
-    //     for (int j=0; j<(int)coordinate[i].size(); j++){
-    //         std::cout << std::setprecision(20) << coordinate[i][j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+    /*貪欲法による経路*/
+    std::vector<int> greedy_city_order = choose_nearest_city(coordinate);
+    /*ファイルに書き込み*/
+    write_file(greedy_city_order, argv[2]);
 }
