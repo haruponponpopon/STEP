f876f708191e16e7533edfa76cb4b0ea2eaf3a19
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-29 10:09:55 +0900

    Update insert.cpp

--- week7/insert.cpp
+++ week7/insert.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <float.h>
 #include <cmath>
+#include <numeric>
+#include <algorithm>
 using namespace std;
 
 struct Point{
@@ -57,32 +59,40 @@ double city_distance(const Point& p1, const Point& p2){
     return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
 }
 
-/*都市0から一番近い都市を返す*/
-int nearest_city_from_zero (const std::vector<Point>& coordinates){
-    double min_distance = DBL_MAX;
-    int min_city;
-    for (int i=1; i<(int)coordinates.size(); i++){
-        double distance = city_distance(coordinates.at(0), coordinates.at(i));
-        cout << i << " " << distance << endl;
-        if (min_distance>distance){
-            min_distance = distance;
-            min_city = i;
-        }
+/*都市0から近い順に都市番号を並べる*/
+std::vector<int> nearest_city_order_from_zero (const std::vector<Point>& coordinates, char instruction){
+    int city_count = (int)coordinates.size();
+    std::vector<double> distance_from_zero(city_count);
+    for (int i=1; i<city_count; i++) distance_from_zero.at(i) = city_distance(coordinates.at(0), coordinates.at(i));
+    std::vector<int> city_check_order(city_count);
+    std::iota(city_check_order.begin(), city_check_order.end(),0);
+    if (instruction == '2'){
+        sort(city_check_order.begin(), city_check_order.end(),[&](int x,int y) {
+            return distance_from_zero[x] < distance_from_zero[y];
+        });
+    }else{
+        sort(city_check_order.begin(), city_check_order.end(),[&](int x,int y) {
+            return distance_from_zero[x] > distance_from_zero[y];
+        });
     }
-    cout << min_city << endl;
-    return min_city;
+    return city_check_order;
 }
 
-std::vector<int> insert_city (const std::vector<Point>& coordinates){
-    double distance_one_to_two = city_distance(coordinates.at(0), coordinates.at(1));
+/*訪れる都市をinsert法によって決める*/
+std::vector<int> insert_city (const std::vector<Point>& coordinates, const std::vector<int>& city_check_order){
+    if (coordinates.size()<2) error("insert_city: number of city is too small.");
+    //初期設定
+    double distance_one_to_two = city_distance(coordinates.at(city_check_order.at(0)), coordinates.at(city_check_order.at(1)));
     struct node *start = (struct node*)malloc(sizeof(struct node));
     struct node *next_start = (struct node*)malloc(sizeof(struct node));
     start->next = next_start;
-    start->city = 0;
+    start->city = city_check_order.at(0);
     start->distance_to_next = distance_one_to_two;
     next_start->next = start;
-    next_start->city = 1;
+    next_start->city = city_check_order.at(1);
     next_start->distance_to_next = distance_one_to_two;
+
+    //都市2から順に線形リストに入れる
     for (int i=2; i<(int)coordinates.size(); i++){
         double min_distance = DBL_MAX;
         struct node *min_node;
@@ -90,7 +100,7 @@ std::vector<int> insert_city (const std::vector<Point>& coordinates){
         bool done = false;
         while (!done || current!=start){
             done = true;
-            double distance = city_distance(coordinates.at(current->city), coordinates.at(i))+city_distance((coordinates.at(i)), coordinates.at(current->next->city))-current->distance_to_next;
+            double distance = city_distance(coordinates.at(current->city), coordinates.at(city_check_order.at(i)))+city_distance((coordinates.at(city_check_order.at(i))), coordinates.at(current->next->city))-current->distance_to_next;
             if (distance<min_distance){
                 min_distance = distance;
                 min_node = current;
@@ -99,11 +109,12 @@ std::vector<int> insert_city (const std::vector<Point>& coordinates){
         }
         struct node *insert_city = (struct node*)malloc(sizeof(struct node));
         insert_city->next = min_node->next;
-        insert_city->city = i;
-        insert_city->distance_to_next = city_distance(coordinates.at(insert_city->next->city), coordinates.at(i));
+        insert_city->city = city_check_order.at(i);
+        insert_city->distance_to_next = city_distance(coordinates.at(insert_city->next->city), coordinates.at(city_check_order.at(i)));
         min_node->next = insert_city;
-        min_node->distance_to_next = city_distance(coordinates.at(min_node->city), coordinates.at(i));
+        min_node->distance_to_next = city_distance(coordinates.at(min_node->city), coordinates.at(city_check_order.at(i)));
     }
+    //線形リストを配列に変換
     struct node *current = start;
     std::vector<int> city_order;
     bool done = false;
@@ -128,11 +139,16 @@ void write_file(const std::vector<int>& city_order, const std::string& file_numb
 }
 
 int main(int argc, char *argv[]){
-    if (argc!=2) error("put any numer");
+    if (argc!=3) error("put any numer and instruction");
     std::string file_number = argv[1];
     /*ファイルの読み込み*/
     std::vector<Point> coordinates = read_file(file_number);
-    std::vector<int> city_order = insert_city(coordinates);
-    /*ファイルに書き込み*/
+    /*線形リストに挿入する都市の順番を決める*/
+    std::vector<int> city_check_order(coordinates.size());
+    if (*argv[2] == '1') std::iota(city_check_order.begin(), city_check_order.end(), 0);
+    else city_check_order = nearest_city_order_from_zero(coordinates, *argv[2]);
+    /*0番目から順にinsertしていく。*/
+    std::vector<int> city_order = insert_city(coordinates, city_check_order);
+    // /*ファイルに書き込み*/
     write_file(city_order, file_number);
 }
